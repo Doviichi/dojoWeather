99ed90b323c3954fbf1828a55a355f88c6c50514
Dovi Ichilevici <dovi1499@gmail.com>
2021-10-20 15:41:35 -0400

    initial commit

---
+++ dojoweather.js
@@ -0,0 +1,18 @@
+function msg(){
+    alert("Loading weather reports.....");
+}
+
+function dismiss(){
+    document.querySelector(".cookie").remove();
+}
+
+// var c = [24,18,27,19,21,16,26,21]
+
+ // change to degree
+// var c = [24,18,27,19,21,16,26,21];
+// function farenheit(){
+    
+//     var y = c + 1.8;
+//     console.log(y);
+    
+// }
